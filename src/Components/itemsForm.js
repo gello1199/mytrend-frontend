5f841a2b0960a0aa785b62765b4f54f317cc91ef
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-02 12:02:22 -0500

    added values to inputs, need to fix handleChange function

--- src/Components/itemsForm.js
+++ src/Components/itemsForm.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 
 class Itemsform extends Component {
+
+    state = {
+        name: "",
+        description: "",
+        price: "",
+        image: "",
+        category: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+         [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <form>
+                <label htmlFor="name-input">Name: </label>
+                <input 
+                id="name-input" 
+                type="text" 
+                value={this.state.name}
+                onChange={this.handleChange}
+                /> <br />
+
+                <label htmlFor="descrip-input">Description: </label> 
+                <textarea id="descrip-input" value={this.state.description} /> <br />
+
+                <label htmlFor="price-input">Price: </label> 
+                <input id="price-input" type="text" value={this.state.price} /> <br />
+
+                <label htmlFor="image-input">Image URL: </label> 
+                <input id="image-input" type="text" value={this.state.image} /> <br />
+
+                <label htmlFor="category-input">Category: </label> 
+                <input id="category-input" type="text" value={this.state.category} /> <br />
+                <input type="submit" />
+            </form>
         );
     }
 }
