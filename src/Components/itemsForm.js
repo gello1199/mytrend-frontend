953fb4fe762fbf3c68ef64979cdbe9060bf81014
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-02 12:15:59 -0500

    abstacted handleChange and added name value to inputs

--- src/Components/itemsForm.js
+++ src/Components/itemsForm.js
@@ -16,11 +16,25 @@ class Itemsform extends Component {
         })
     }
 
+    handleSubmit = event => {
+        event.preventDefault()
+
+        this.setState({
+            name: "",
+            description: "",
+            price: "",
+            image: "",
+            category: ""
+        })
+    }
+
     render() {
+        // console.log(this.state)
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label htmlFor="name-input">Name: </label>
                 <input 
+                name="name"
                 id="name-input" 
                 type="text" 
                 value={this.state.name}
@@ -28,16 +42,40 @@ class Itemsform extends Component {
                 /> <br />
 
                 <label htmlFor="descrip-input">Description: </label> 
-                <textarea id="descrip-input" value={this.state.description} /> <br />
+                <textarea 
+                name="description"
+                id="descrip-input" 
+                value={this.state.description}
+                onChange={this.handleChange}
+                /> <br />
 
                 <label htmlFor="price-input">Price: </label> 
-                <input id="price-input" type="text" value={this.state.price} /> <br />
+                <input 
+                name="price"
+                id="price-input" 
+                type="text" 
+                value={this.state.price}
+                onChange={this.handleChange}
+                /> <br />
 
                 <label htmlFor="image-input">Image URL: </label> 
-                <input id="image-input" type="text" value={this.state.image} /> <br />
+                <input 
+                name="image"
+                id="image-input" 
+                type="text" 
+                value={this.state.image}
+                onChange={this.handleChange} 
+                /> <br />
 
                 <label htmlFor="category-input">Category: </label> 
-                <input id="category-input" type="text" value={this.state.category} /> <br />
+                <input 
+                name="category"
+                id="category-input" 
+                type="text" 
+                value={this.state.category} 
+                onChange={this.handleChange}
+                /> <br />
+                
                 <input type="submit" />
             </form>
         );
