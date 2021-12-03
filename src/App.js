82b053882e9907098b21f29c15a74de008b85ae2
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-03 17:59:31 -0500

    Added grey to selected links

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <div>
       <div className="nav-links">
-        <NavLink to='/home'>Home</NavLink>
-        <NavLink to='/trends'>Trends</NavLink> 
-        <NavLink to='/new'>Add A New Look</NavLink>
-        <NavLink to='/favorites'>Favorites</NavLink>
+        <NavLink to='/home' className={isActive => isActive ? "selected" : ""}>Home</NavLink>
+        <NavLink to='/trends' className={isActive => isActive ? "selected" : ""}>Trends</NavLink> 
+        <NavLink to='/new' className={isActive => isActive ? "selected" : ""}>Add A New Look</NavLink>
+        <NavLink to='/favorites' className={isActive => isActive ? "selected" : ""}>Favorites</NavLink>
       </div>
       <Navbar />
     </div>
--- src/Components/Items/Forms/itemsForm.js
+++ src/Components/Items/Forms/itemsForm.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { addItem } from '../../../Redux/actions/items';
 
 class ItemsForm extends Component {
-
+    
     state = {
         name: "",
         description: "",
