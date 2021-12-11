dab21753e34ed8c578c98618948b110c2d55b4b2
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-11 17:47:38 -0500

    added logo to homepage

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -4,9 +4,9 @@ import LogoTransparent from '../images/logo_transparent.png'
 const Welcomepage = () => {
     return (
         <div>
+            <img src={LogoTransparent} alt="logo" className="logo" />
             <h1>Welcome to MyTrends</h1>
             <p>Here is a one stop shop where you can curate your favorite looks from our trends page!</p>
-            <img src={LogoTransparent} alt="logo" className="logo" height="200" width="200" />
             
         </div>);
 }
