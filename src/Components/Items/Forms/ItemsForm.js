e02026f018c654abc97ed2664aaa8a4c020d07e6
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-14 12:55:09 -0500

    ignore case

---
+++ src/Components/Items/Forms/ItemsForm.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../../Redux/actions/items';
+
+class ItemsForm extends Component {
+
+    state = {
+        name: "",
+        description: "",
+        price: "",
+        image: "",
+        category: "men",
+    }
+
+    handleChange = (event) => {
+        this.setState({
+         [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        // console.log(this.props)
+        event.preventDefault()
+
+        this.props.dispatchAddItem(this.state, this.props.routerHistory)
+
+        this.setState({
+                name: "",
+                description: "",
+                price: "",
+                image: "",
+                category: "men",
+        })
+    }
+
+    render() {
+        // console.log(this.state)
+        return (
+            <div className="item-form">
+            <h3>Create A New Look</h3>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                <input 
+                name="name"
+                id="name-input" 
+                type="text" 
+                value={this.state.name}
+                onChange={this.handleChange}
+                placeholder="Name"
+                /> <br />
+                </div>
+
+                <div>
+                <textarea 
+                name="description"
+                id="descrip-input" 
+                value={this.state.description}
+                onChange={this.handleChange}
+                placeholder="Description"
+                /> <br />
+                </div>
+
+                <input 
+                name="price"
+                id="price-input" 
+                type="text" 
+                value={this.state.price}
+                onChange={this.handleChange}
+                placeholder="Price"
+                /> <br />
+
+                <input 
+                name="image"
+                id="image-input" 
+                type="text" 
+                value={this.state.image}
+                onChange={this.handleChange} 
+                placeholder="Image URL"
+                /> <br />
+
+                <label htmlFor="category">Select a Category:</label>
+                <select 
+                name="category" 
+                id="category-input"
+                onChange={this.handleChange}
+                value={this.state.category} >
+                <option value="men">Men</option>
+                <option value="women">Women</option>
+                </select>
+                <br />
+                
+                <input className="button" type="submit" />
+            </form>
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+    dispatchAddItem: (item, redirect) => dispatch(addItem(item, redirect))
+    }
+}
+
+export default connect(null, mapDispatchToProps) (ItemsForm);
+
---
+++ src/Components/Items/Item/ItemCard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { editFavorites, deleteItem } from '../../../Redux/actions/items';
+import { connect } from 'react-redux';
+
+const ItemCard = (props) => {
+    // console.log(props)
+
+    function handleDelete() {
+        // console.log(typeof props.id)
+        props.dispatchDeleteItem(props.id)
+    }
+
+    function handleFavorite() {
+        // console.log(props)
+        props.dispatchAddToFavorites(props)
+    }
+
+    return (
+       
+        <div key={props.id} className="item-card">
+           Name: {props.name} <br />
+           Description: {props.description} <br />
+           Price: ${props.price} <br />
+            
+           <button 
+           className="button" 
+           onClick={handleFavorite}>
+           {props.favorite ? "Remove From Favorites" : "Add To Favorites"} 
+           </button> 
+
+            {props.location ? "" : 
+            <button 
+            className="button" 
+            onClick={handleDelete}>
+            Delete Item</button> 
+            }
+            
+            <br />
+            
+           <img src={props.image} alt="trends" className="image" />
+        </div>
+        
+    );
+
+}
+
+
+const mapDispatchToProps = dispatch => ({
+    dispatchDeleteItem: (id) => dispatch( deleteItem(id) ),
+    dispatchAddToFavorites: (id) => dispatch( editFavorites(id) )
+})
+
+export default connect(null, mapDispatchToProps)(ItemCard);
